7df0830ca0171f140e5e5870541b489244abc221
xyangp <67683433+xyangp@users.noreply.github.com>
2021-04-20 16:55:08 +0800

    Add files via upload

---
+++ testScript.js
@@ -0,0 +1,70 @@
+// Import the express library
+const express = require("express");
+const body_parser = require("body-parser");
+const cors = require("cors"); // this to tell server and frontend to allow running from the same machine
+const accounts = require("./accounts"); //import accounts.js file
+
+// 
+service = express();
+service.use(cors());
+service.use(body_parser.json());
+// app.use(bodyParser.urlencoded());
+service.use(accounts.router);
+
+// We start the service on port 3000. API Listener has started on port 3000.
+// service.listen(3000);
+
+///////////////////////////////////////////////////////
+
+var AccountsDatabase;
+var AccountsData;
+function getFromServer(){
+  $.getJSON('https://583d562a-3646-4c43-ae04-47aff88479b3.mock.pstmn.io/database', function(database) {      
+    AccountsDatabase = database;
+  });
+  setTimeout(delayAccountsData(),1000);
+}
+
+// Add transactions to DOM list
+function addTransactionDOM(account) {
+  const account_item = document.createElement('li');
+  account_item.classList.add('plus');
+  account_item.innerHTML = `
+  ${account.first_name} ${account.last_name} - 
+  ${account.fi_name}  <span> 
+  $ ${account.account_balance}</span> `
+  list.appendChild(account_item);
+
+}
+
+// Update the balance, deposit and loan
+function updateValues() {
+  const bal = AccountsData.map(account => account.account_balance);
+  const total_balance = bal.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  money_plus.innerText = `$${total_balance}`;
+}
+
+function filterTransaction(e) {
+  e.preventDefault();  //to prevent form from submitting and refreshing the page
+  list.innerHTML = '';
+  reco.innerHTML = '';
+  AccountsData = AccountsDatabase.filter(account => account.first_name == custname.value);  
+  AccountsData.forEach(addTransactionDOM);
+  updateValues(); 
+}
+
+// delay AccountsData
+function delayAccountsData() {
+  AccountsData = AccountsDatabase;
+}
+
+function init() {
+  getFromServer();
+  list.innerHTML = '';
+  reco.innerHTML = '';
+  AccountsData.forEach(addTransactionDOM);
+  updateValues();
+}
+
+init();
+// form.addEventListener('submit', filterTransaction);
